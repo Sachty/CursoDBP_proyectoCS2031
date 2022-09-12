9dc29f3d5029c8f082e1a672e9ca7998d386eb9c
Allyson <79607941+Sachty@users.noreply.github.com>
2022-09-12 11:00:15 -0500

    EjercicioClase_3
    Añadi codigo en archivos para poder actualizar datos del usuario.

--- app/controllers/ProfileController.py
+++ app/controllers/ProfileController.py
@@ -31,3 +31,29 @@ def delete():
         return redirect("/index")
 
     return render_template("delete.html")
+
+
+# FUNCION: Actualizar usuario
+def update():
+    if request.method == "POST":
+        new_username = request.form["username"]
+        new_password = request.form["password"]
+
+        if new_username == None or new_username == "" or new_password == None or new_password == "":
+            return "Invalidad updated parameters"
+
+        newUser = NewUser.query.filter(NewUser.username == new_username).first()
+
+        if newUser == None or newUser.password != new_password:
+            return "User does not exist or password does not match."
+
+
+        try:
+            db.session.update(newUser)
+            db.session.commit()
+        except Exception as err:
+            print(err)
+            return ("Internal server error.")
+        return redirect("/profile") # Redirigir usuario a la pagina "profile"
+
+    #return render_template("profile.html")   
--- app/routes/profile_bp.py
+++ app/routes/profile_bp.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
-from app.controllers.ProfileController import profile, delete
+from app.controllers.ProfileController import profile, delete, update
 
 profile_bp = Blueprint('profile_bp', __name__)
 
 profile_bp.route('/', methods=["GET"]) (profile)
 profile_bp.route('/delete', methods=["GET", "POST"]) (delete)
+
+# Agregando ruta para actualizar datos
+profile_bp.route('/update', methods=["GET", "POST"]) (update)
